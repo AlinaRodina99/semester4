522f114fbacdbcee5cdf57074eb652c169816447
alnardn207 <alnardn207@gmail.com>
2020-05-09 01:38:44 +0300

    added more comments

--- homework2/hw2.1/hw2.1/MapCount.fs
+++ homework2/hw2.1/hw2.1/MapCount.fs
@@ -1,3 +1,4 @@
 ﻿module MapCount
 
+///<summary> Function to count even numbers in list using map function.</summary>
 let mapCount list = list |> List.map (fun x -> 1 - abs(x) % 2) |> List.fold (+) 0
