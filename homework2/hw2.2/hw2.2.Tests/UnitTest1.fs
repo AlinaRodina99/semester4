859d55693fb01f082913a052dc0106cea58dae8f
alnardn207 <alnardn207@gmail.com>
2020-05-09 18:07:30 +0300

    added comments, started writing tests

--- homework2/hw2.2/hw2.2.Tests/UnitTest1.fs
+++ homework2/hw2.2/hw2.2.Tests/UnitTest1.fs
@@ -1,11 +1,9 @@
 module hw2._2.Tests
 
 open NUnit.Framework
-
-[<SetUp>]
-let Setup () =
-    ()
+open FsUnit
+open MapTree
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``Test1`` () =
+    mapTree (fun x -> x * 2) (Node(0, Node(1, Node(2, Tip, Tip), Node(3, Tip, Tip)), Node(4, Tip, Tip))) |> should equal (Node(0, Node(2, Node(4, Tip, Tip), Node(6, Tip, Tip)), Node(8, Tip, Tip)))
--- homework2/hw2.2/hw2.2/MapTree.fs
+++ homework2/hw2.2/hw2.2/MapTree.fs
@@ -1,2 +1,12 @@
 ﻿module MapTree
 
+///<summary> Representation of the binary tree as discrimanted union.</summary>
+type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Tip
+
+///<summary> Function that applies inner function to each element of the tree and makes new tree.</summary>
+let rec mapTree f tree =
+    match tree with 
+    | Tip -> Tip
+    | Node(value, left, right) -> Node(f value, mapTree f left, mapTree f right)
