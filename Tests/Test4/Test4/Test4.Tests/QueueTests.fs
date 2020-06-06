37d9c4756b09a5f168dd5572e64e7149a4089e83
alnardn207 <alnardn207@gmail.com>
2020-06-06 18:03:33 +0300

    minor changes

--- Tests/Test4/Test4/Test4.Tests/QueueTests.fs
+++ Tests/Test4/Test4/Test4.Tests/QueueTests.fs
@@ -17,18 +17,9 @@ let ``test isEmpty on empty queue`` () =
     queue.IsEmpty () |> should equal true
 
 [<Test>]
-let ``tests to check whether enqueue method is working`` () =
-     queue.Enqueue 5 |> ignore
-     queue.Enqueue 20 |> ignore
-     queue.Enqueue 30 |> ignore
-     (fun () -> queue.Enqueue 5 |> ignore) |> should throw typeof<ArgumentException>
-     (fun () -> queue.Enqueue 20 |> ignore) |> should throw typeof<ArgumentException>
-     (fun () -> queue.Enqueue 30 |> ignore) |> should throw typeof<ArgumentException>
-
-[<Test>]
 let ``tests to check whether dequeue method is working`` () =
     queue.Enqueue 2 
     queue.Enqueue 3 
     queue.Dequeue |> should equal 2
     queue.Dequeue |> should equal 3
-    (fun () -> queue.Dequeue |> ignore) |> should throw typeof<ArgumentException>
+    queue.IsEmpty () |> should equal true
