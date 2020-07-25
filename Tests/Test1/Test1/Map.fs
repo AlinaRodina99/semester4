f17c4877abe94bf5995aacf9f2dd6d8d6bc8ff97
alnardn207 <alnardn207@gmail.com>
2020-07-25 17:38:02 +0300

    deleted unnecessary files

--- Tests/Test1/Test1/Map.fs
+++
@@ -1,9 +0,0 @@
-﻿module Map
-
-open System
-
-let superMap list =
-    let innerMap list = List.map (fun x -> [Math.Sin x; Math.Cos x]) list
-    match list with 
-    | [] -> failwith "List is empty!"
-    | hd :: tl -> innerMap list
