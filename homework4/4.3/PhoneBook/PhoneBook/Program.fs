f7e23af963880d6f4bcc384993127de7f6994713
alnardn207 <alnardn207@gmail.com>
2020-07-31 20:57:03 +0300

    made function to interact with user, added comments

--- homework4/4.3/PhoneBook/PhoneBook/Program.fs
+++ homework4/4.3/PhoneBook/PhoneBook/Program.fs
@@ -1,8 +1,42 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open phoneBook
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main =
+    printfn "Hello! This is a phone book. Here's how you can use it: "
+    printfn "1 - exit"
+    printfn "2 - add new contact (name and phone)"
+    printfn "3 - find phone by name"
+    printfn "4 - find name by phone"
+    printfn "5 - show database of phone book"
+    printfn "6 - save current database to file on the specified path"
+    printfn "7 - read data from file on the specified path"
+    let rec interactWithUser list =
+        printfn "Input your command: "
+        let input = Convert.ToInt32(Console.ReadLine())
+        match input with
+        | 1 ->  printfn "Goodbye!"
+        | 2 ->  printfn "Input name of your contact:"
+                let name = Convert.ToString(Console.ReadLine())
+                printfn "Input phone of your contact:"
+                let phone = Convert.ToString(Console.ReadLine())
+                let list = addNewContact list name phone
+                interactWithUser list
+        | 3 ->  printfn "Input name of your contact:"
+                let name = Convert.ToString(Console.ReadLine())
+                printfn "Phone of the contact: %A" (findPhoneByName list name)
+                interactWithUser list
+        | 4 ->  printfn "Input phone of your contact:"
+                let phone = Convert.ToString(Console.ReadLine())
+                printfn "Name of the contact: %A" (findNameByPhone list phone)
+                interactWithUser list
+        | 5 ->  showDataBaseOfPhoneBook list
+                interactWithUser list 
+        | _ ->  printfn "Choose commands from 1 to 7"
+                interactWithUser list
+    interactWithUser []
+
+
     0 // return an integer exit code
--- homework4/4.3/PhoneBook/PhoneBook/phoneBook.fs
+++ homework4/4.3/PhoneBook/PhoneBook/phoneBook.fs
@@ -1,9 +1,9 @@
 ﻿module phoneBook
 
-open System.IO
-
+/// Function to add new contact in phone book.
 let addNewContact (list : (string * string) list) (name : string) (phone : string) = (name, phone) :: list
 
+/// Function to find phone by name of the contact.
 let findPhoneByName (list : (string * string) list) (name : string) =
     let rec findPhoneByNameRec currentList =
         match currentList with 
@@ -12,6 +12,7 @@ let findPhoneByName (list : (string * string) list) (name : string) =
         | _ -> printfn "This person does not exist!"
     findPhoneByNameRec list
 
+/// Function to find name by phone of the contact.
 let findNameByPhone (list : (string * string) list) (phone : string) =
     let rec findNameByPhoneRec currentList =
         match currentList with
@@ -20,10 +21,11 @@ let findNameByPhone (list : (string * string) list) (phone : string) =
         | _ -> printfn "This phone does not exist!"
     findNameByPhoneRec list
 
+/// Function to print database of phone book.
 let showDataBaseOfPhoneBook (list : (string * string) list) =
     let rec showDataBaseOfPhoneBookRec currentList =
         match currentList with 
-        | hd :: tl -> printfn "%A" hd
+        | hd :: tl -> printfn "Name: %A Phone: %A" (fst hd) (snd hd) 
                       showDataBaseOfPhoneBookRec tl
         | _ -> printfn "Thats all"
     showDataBaseOfPhoneBookRec list
