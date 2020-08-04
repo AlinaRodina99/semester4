d0a8ad49a75596da7d2cf6057bf2ec2f8b561d92
alnardn207 <alnardn207@gmail.com>
2020-08-04 19:35:03 +0300

    added one more test, corrected menu of the phone book

--- homework4/4.3/PhoneBook/PhoneBook.Tests/Tests.fs
+++ homework4/4.3/PhoneBook/PhoneBook.Tests/Tests.fs
@@ -1,8 +1,12 @@
-module PhoneBook.Tests
+module Tests
 
 open NUnit.Framework
 open phoneBookFunctions
 open FsUnit
+open System
+open System.IO
+
+let path = "../../../test.txt"
 
 [<Test>]
 let ``check that adding new contact is working`` () =
@@ -22,12 +26,26 @@ let ``check that finding name by phone is working`` () =
     name3 |> should equal (Some("Sam"))
 
 [<Test>]
-let ``chack that finding phone by name is working`` () =
+let ``check that finding phone by name is working`` () =
     let book = addNewContact [] "Kate" "123456" |> List.append (addNewContact [] "Liza" "666-666")
     let phone1 = findPhoneByName book "Kate"
     phone1 |> should equal (Some("123456"))
     let phone2 = findPhoneByName book "Liza"
     phone2 |> should equal (Some("666-666"))
+
+[<Test>]
+let ``check that reading from file is working`` () =
+    let book = readCurrentDatabaseFromFile path1 |> List.map (fun str -> str.Split())
+               |> List.map (fun str -> Array.pathist str)
+    List.item 0 book |> should equal ["Bob"; "555-555"]
+    List.item 1 book |> should equal ["Kate"; "65-78-99"]
+    List.item 2 book |> should equal ["Sam"; "66-78-90"]
+
+[<Test>]
+let ``check that user can not add existing contact`` () =
+    let book = [("Bob", "555-555"); ("Sam", "666-666")] 
+    let newBook = addNewContact [("Bob", "555-555"); ("Sam", "666-666")] "Bob" "555-555"
+    newBook |> should equal book
 
 
 
