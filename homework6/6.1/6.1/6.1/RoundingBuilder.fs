4df83967be301931f7d4265a112a6841c975d5de
alnardn207 <alnardn207@gmail.com>
2020-08-05 18:43:44 +0300

    added 6.1

---
+++ homework6/6.1/6.1/6.1/RoundingBuilder.fs
@@ -0,0 +1,21 @@
+﻿module RoundingBuilder
+
+open System
+
+let divide x y (b : int) =
+    match y with
+    | 0.0 -> None
+    | _ -> Some(Math.Round(Some(x/y).Value, b))
+
+type RoundingBuilder(a : int) =
+     member _.Bind(x, f) =
+        match x with 
+        | None -> None
+        | Some a -> f a
+        
+     member _.Return(x) =
+        Some(x)
+
+let roundingBuilder = new RoundingBuilder(3)
+
+let rounding 
