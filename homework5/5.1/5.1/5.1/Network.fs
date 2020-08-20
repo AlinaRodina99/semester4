8f2b139845e627ac087dfafec1369cf7421f7a18
unknown <alnardn207@gmail.com>
2020-08-20 12:35:00 +0300

    minor changes

---
+++ homework5/5.1/5.1/5.1/Network.fs
@@ -0,0 +1,35 @@
+﻿module Network
+
+open Computer
+
+type Network (computers : Computer list, matrix : bool [,]) =
+
+     let stepOfInfection = 
+        List.iter (fun (x : Computer) -> 
+        if x.IsInfected then (List.iter (fun (y : Computer) -> 
+           if (x.Infect(y.GetProbabilityByOS)) then (y.IsInfected <- true)
+           else (y.IsInfected <- true))) x.ListOfRelatedComputers
+        else ())
+
+     let fillListsOfRelatedComputers =
+        let lengthOfComputerList = List.length computers
+        for i in 0 .. (lengthOfComputerList - 1) do
+           for j in 0 .. (lengthOfComputerList - 1) do
+              if matrix.[i, j] = true then ((computers.Item i).ListOfRelatedComputers) <- (computers.Item j) :: ((computers.Item i).ListOfRelatedComputers)
+              else ()
+
+     let printStateOfNetwork =
+        for computer in computers do
+           printf "Name of computer: %A, operation system: %A, state of infection: " computer.Name computer.OperationSystem 
+           if computer.IsInfected then printfn "infected" else printfn "uninfected" 
+
+     member _.Work (amountOfSteps : int) = 
+         fillListsOfRelatedComputers
+         stepOfInfection
+
+     member _.PrintStateOfNetwork = printStateOfNetwork
+
+     member _.Computers = computers
+
+     
+     
---
+++ homework5/5.1/5.1/5.1/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Computer
+
+[<EntryPoint>]
+let main argv =
+    let computer1 = new Computer("Windows", "Azus", false)
+    0 // return an integer exit code
