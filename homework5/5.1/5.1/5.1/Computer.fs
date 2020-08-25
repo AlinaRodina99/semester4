926c6934dbaac1e614160bac155fad0d0153c6a6
unknown <alnardn207@gmail.com>
2020-08-25 17:01:45 +0300

    Network

---
+++ homework5/5.1/5.1/5.1/Computer.fs
@@ -0,0 +1,32 @@
+﻿module Computer
+
+open System
+
+///Class that describes computer view.
+type Computer (operationSystem : string, name : string, isInfected : bool) =
+    
+     /// Current state of infection.
+     let mutable isInfected = isInfected
+
+     /// Possiblity of infection depending on operation system.
+     member _.GetProbabilityByOS = 
+          match operationSystem with
+          | "Windows" -> 1.0
+          | "MacOS" -> 0.2
+          | "Linux" -> 0.0
+          | _ -> 0.5
+
+     member _.OperationSystem = operationSystem
+
+     /// Method to infect computers that are connected to current.
+     member _.Infect (probability : float) = 
+          let rnd = new Random()
+          let randomValue = rnd.NextDouble()
+          if randomValue <  probability then true else false
+    
+     member _.IsInfected 
+         with get () = isInfected
+         and set (value) = isInfected <- value
+     
+     /// Name of computer.
+     member val Name = name with get 
---
+++ homework5/5.1/5.1/5.1/Network.fs
@@ -0,0 +1,33 @@
+﻿module Network
+
+open Computer
+
+/// Class that describes working of net.
+type Network (computers : Computer list, matrix : bool [,]) =
+     
+     /// Varibale for lentgh of computer list that is used in infect function.
+     let lentghOfComputerList = List.length computers
+
+     /// Function to infect computers for one iteraion.
+     let stepOfInfection () = 
+        for i in 0 .. (lentghOfComputerList - 1) do
+          for j in 0 .. (lentghOfComputerList - 1) do
+             if matrix.[i, j] = true && (computers.Item i).IsInfected then
+               if (not (computers.Item j).IsInfected) && (computers.Item j).Infect((computers.Item j).GetProbabilityByOS) then
+                  (computers.Item j).IsInfected <- true
+                  else ()
+     
+     /// Function to print current state of net.
+     let printStateOfNetwork () =
+        for computer in computers do
+           printf "Name of computer: %A, operation system: %A, state of infection: " computer.Name computer.OperationSystem 
+           if computer.IsInfected then printfn "infected" else printfn "uninfected" 
+     
+     /// Working of net, amountOfSteps - amount of iterations while net is working.
+     member _.Work (amountOfSteps) =
+         for i in 1 .. amountOfSteps do
+             stepOfInfection()
+             if i % 2 = 0 then printStateOfNetwork()
+     
+     /// Computers in net.
+     member _.Computers = computers
