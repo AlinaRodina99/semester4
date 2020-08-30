524ac4281e4145fcf6db65a6bb61483958b45f4e
unknown <alnardn207@gmail.com>
2020-08-30 17:10:57 +0300

    made auto properties of IsInfected and NewlyInfected

--- homework5/5.1/5.1/5.1/Computer.fs
+++ homework5/5.1/5.1/5.1/Computer.fs
@@ -6,35 +6,31 @@ open OperationSystem
 ///Class that describes computer view.
 type Computer (operationSystem : OperationSystem, name : string, isInfected : bool) =
     
-     /// Variable to know if computer was infected.
-     let mutable isInfected = isInfected
-
-     /// Variable to know if computer was infected on current iteration.
-     let mutable newlyInfected = false
-
+     /// Operation system of computer.
      member _.OperationSystem = operationSystem
+
+     /// Property to know that computer was just infected.
+     member val NewlyInfected = false with get, set
      
      /// Property to know that current computer is infected.
-     member _.IsInfected 
-         with get () = isInfected
-         and set (value) = isInfected <- value
+     member val IsInfected = isInfected with get, set
      
      /// Name of computer.
      member val Name = name with get 
 
      /// Method to know if computer can infect related computer.
-     member _.CanInfect () = (isInfected) && (newlyInfected = false)
+     member this.CanInfect () = this.IsInfected && (this.NewlyInfected = false)
 
      /// Method to know if uninfected computer can be infected.
      member _.CanBeInfected (random : Random) =
          random.NextDouble() < operationSystem.GetProbabilityByOS
 
      /// Method to change status of infection of recently infected to an already infected.
-     member _.ClearNewlyInfectedComputer () =
-         if newlyInfected then (newlyInfected <- false)
+     member this.ClearNewlyInfectedComputer () =
+         if this.NewlyInfected then (this.NewlyInfected <- false)
 
      /// Methos of computer infection.
-     member _.Infect () =
-         isInfected <- true
-         newlyInfected <- true
+     member this.Infect () =
+         this.IsInfected <- true
+         this.NewlyInfected <- true
          
