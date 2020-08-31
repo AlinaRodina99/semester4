d9649722629f60240afbca90e472e721b7ab232c
unknown <alnardn207@gmail.com>
2020-08-31 14:16:17 +0300

    added new files

---
+++ homework6/hw_6/hw_6/hw_6.Tests/RoundTests.fs
@@ -0,0 +1,41 @@
+module Tests
+
+open NUnit.Framework
+open FsUnit
+open RoundingBuilder
+
+[<Test>]
+let ``test from example`` () =
+  let rounding = new RoundingBuilder(3)
+  rounding {
+     let! a = 2.0 / 12.0
+     let! b = 3.5
+     return a / b
+  } |> should equal 0.048
+
+[<Test>]
+let ``simple test 1`` () =
+  let rounding = new RoundingBuilder(4)
+  rounding {
+     let! a = 3.0 / 5.0
+     let! b = 3.45 * 6.8
+     return a / b
+  } |> should equal 0.0256
+
+[<Test>]
+let ``simple test 2 with negative numbers`` () =
+  let rounding = new RoundingBuilder(2)
+  rounding {
+     let! a = -5.0 / 4.0
+     let! b = 6.0
+     return a * b
+  } |> should equal -7.50
+
+[<Test>]
+let ``simple test 3`` () =
+  let rounding = new RoundingBuilder(3)
+  rounding {
+     let! a = 6.0 * 8.67
+     let! b = 7.0
+     return a / b
+  } |> should equal 7.431
---
+++ homework6/hw_6/hw_6/hw_6/RoundingBuilder.fs
@@ -0,0 +1,11 @@
+﻿module RoundingBuilder
+
+open System
+
+/// Workflow that does computations with numbers as strings.
+type RoundingBuilder(accuracy : int) =
+     member _.Bind(x, f) =
+         Math.Round((x : float), accuracy) |> f
+        
+     member _.Return(x) =
+         Math.Round((x : float), accuracy)
