90e477aedfbbd20b3f14fa998589fe70119adf59
unknown <alnardn207@gmail.com>
2020-08-31 14:18:44 +0300

    deleted wrong files

--- homework5/5.1/5.1/5.1/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open Computer
-
-[<EntryPoint>]
-let main argv =
-    let computer1 = new Computer("Windows", "Azus", false)
-    0 // return an integer exit code
