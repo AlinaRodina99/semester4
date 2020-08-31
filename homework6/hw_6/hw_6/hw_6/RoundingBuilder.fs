8981cc88befc0ac971b86952a4e5fd6c9a3e6a04
unknown <alnardn207@gmail.com>
2020-08-31 14:20:29 +0300

    deleted unnecessary lines of code

--- homework6/hw_6/hw_6/hw_6/RoundingBuilder.fs
+++ homework6/hw_6/hw_6/hw_6/RoundingBuilder.fs
@@ -4,6 +4,7 @@ open System
 
 /// Workflow that does computations with numbers as strings.
 type RoundingBuilder(accuracy : int) =
+     
      member _.Bind(x, f) =
          Math.Round((x : float), accuracy) |> f
         
